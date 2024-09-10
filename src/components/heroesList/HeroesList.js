ee9ed9e62f89eb90c218651b63336a1e987a1368
mahout <93513481+muhahoi@users.noreply.github.com>
2024-09-10 17:03:15 +0300

    commit

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHttp } from '../../hooks/http.hook';
-// import { HeroesFilters } from '../heroesFilters/HeroesFilters';
 
 import {
 	heroDeleting,
