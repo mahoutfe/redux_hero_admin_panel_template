88689c8b68a6c7955a8de93972661be6433644e2
mahout <93513481+muhahoi@users.noreply.github.com>
2024-09-20 12:55:49 +0300

    middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,32 @@
-import { combineReducers, compose, createStore } from 'redux';
+import { applyMiddleware, combineReducers, compose, createStore } from 'redux';
 import filters from '../reducers/filters';
 import heroes from '../reducers/heroes';
 
-const enhancer =
-	(createStore) =>
-	(...args) => {
-		const store = createStore(...args);
+const stringMiddleware = (store) => (next) => (action) => {
+	if (typeof action === 'string') {
+		return next({ type: action });
+	}
+	return next(action);
+};
 
-		const oldDispatch = store.dispatch;
-		store.dispatch = (action) => {
-			if (typeof action === 'string') {
-				return oldDispatch({ type: action });
-			}
-			return oldDispatch(action);
-		};
-		return store;
-	};
+// const enhancer =
+// 	(createStore) =>
+// 	(...args) => {
+// 		const store = createStore(...args);
+
+// 		const oldDispatch = store.dispatch;
+// 		store.dispatch = (action) => {
+// 			if (typeof action === 'string') {
+// 				return oldDispatch({ type: action });
+// 			}
+// 			return oldDispatch(action);
+// 		};
+// 		return store;
+// 	};
 const store = createStore(
 	combineReducers({ heroes, filters }),
 	compose(
-		enhancer,
+		applyMiddleware(stringMiddleware),
 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	)
 );
