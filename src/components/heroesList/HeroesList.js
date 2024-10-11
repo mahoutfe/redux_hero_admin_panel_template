b86f5c3da66bc934607e7ff19299f7b94c950588
mahout <93513481+muhahoi@users.noreply.github.com>
2024-10-11 09:11:23 +0300

    heroesAdapter

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,26 +1,18 @@
-import { createSelector } from '@reduxjs/toolkit';
 import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { useHttp } from '../../hooks/http.hook';
 import HeroesListItem from '../heroesListItem/HeroesListItem';
 import Spinner from '../spinner/Spinner';
-import { fetchHeroes, heroDeleted } from './heroesSlice';
+import {
+	fetchHeroes,
+	heroDeleted,
+	filteredHeroesSelector,
+} from './heroesSlice';
 
 import './heroesList.scss';
 
 const HeroesList = () => {
-	const filteredHeroesSelector = createSelector(
-		(state) => state.filters.activeFilter,
-		(state) => state.heroes.heroes,
-		(filter, heroes) => {
-			if (filter === 'all') {
-				return heroes;
-			} else {
-				return heroes.filter((item) => item.element === filter);
-			}
-		}
-	);
 	const filteredHeroes = useSelector(filteredHeroesSelector);
 
 	const heroesLoadingStatus = useSelector(
